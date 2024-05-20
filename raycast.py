b9a29897bb192742e57ab2ac00081e4c305795f9
Justin Barker <jstnbarker@pm.me>
2024-05-19 22:35:21 -0400

    brute force

--- raycast.py
+++ raycast.py
@@ -10,26 +10,8 @@ pygame.mouse.set_visible(0)
 
 walls = []
 
-class LineSpecs:
-    xy1: pygame.Vector2
-    xy2: pygame.Vector2
-    m: float
-    b: float
-    domain = list[int]
+bg = Color(26,26,26)
 
-    def __init__(self, xy1: pygame.Vector2, xy2: pygame.Vector2):
-        x1 = xy1[0]
-        y1 = xy1[1]
-        x2 = xy2[0]
-        y2 = xy2[1]
-
-        self.xy1 = xy1
-        self.xy2 = xy2
-        self.m = (y2 - y1) / (x2 - x1)
-        self.b = y1 - (x1*self.m)
-        self.domain = [x1, x2]
-        self.domain.sort()
-        
 def main():
     tempwall = []
     while True:
@@ -48,41 +30,37 @@ def main():
                 mouse_pos = event.dict["pos"]
                 tempwall.append(pygame.Vector2(mouse_pos))
                 if len(tempwall) == 2:
-                    walls.append(LineSpecs(tempwall[0], tempwall[1]))
-                    print(walls[len(walls)-1].domain)
+                    walls.append([tempwall[0], tempwall[1]])
                     tempwall = []
-            print(event)
             
-        screen.fill("gray")
+        screen.fill(bg)
         mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
 
         for wall in walls:
-            pygame.draw.line(screen, "red", wall.xy1, wall.xy2,width=3)
+            pygame.draw.line(screen, "red", wall[0], wall[1],width=3)
 
-        for deg in range(360):
+        walls_only= screen.copy()
+
+
+        for deg in range(0,360,3):
             ray_x = math.cos(math.radians(deg))*screen.get_width()
-            ray_y = math.sin(math.radians(deg))*screen.get_height()
-            m = ray_y/ray_x
-            b = ray_y-(ray_x*m)
-
-            for wall in walls:
-                x1 = wall.xy1[0] - mouse_pos[0]
-                y1 = wall.xy1[1] - mouse_pos[1]
-                x2 = wall.xy2[0]- mouse_pos[0]
-                y2 = wall.xy2[1]- mouse_pos[1]
-
-                wm = (y2 - y1) / (x2 - x1)
-                wb = (y1-(x1*wm))
-                
-                mouse_rel_intercept = (b - wb)/(wm - m)
-                intercept = mouse_pos[0] + mouse_rel_intercept
-                if( wall.domain[0] <= intercept and intercept <= wall.domain[1]):
-                    #sohcahtoa
-                    ray_x = mouse_rel_intercept
-                    ray_y = mouse_rel_intercept * math.tan(math.radians(deg))
-            pygame.draw.line(screen, "white", mouse_pos,[
-                ray_x+mouse_pos[0], 
-                ray_y+mouse_pos[1] 
+            ray_y = math.sin(math.radians(deg))*screen.get_width()
+            # soh cah toa
+            hyp = 0
+            while True:
+                hyp+=1
+                ray_x = math.cos(math.radians(deg))*hyp
+                ray_y = math.sin(math.radians(deg))*hyp
+                if mouse_pos[0]+ray_x >= screen.get_width() or mouse_pos[0]+ray_x<0:
+                    break
+                if mouse_pos[1]+ray_y >= screen.get_height() or mouse_pos[1]+ray_y < 0:
+                    break
+                endpoint=((int(mouse_pos[0]+ray_x),int(mouse_pos[1]+ray_y)))
+                if walls_only.get_at(endpoint) != bg:
+                    break
+            pygame.draw.line(screen, "white", mouse_pos, [
+                mouse_pos[0]+ray_x,
+                mouse_pos[1]+ray_y
             ])
 
         pygame.draw.circle(screen, "white", mouse_pos, radius=10)
